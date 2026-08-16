67df23d15472fbb27bad9af5e897c93ccb481b03
Dennis Lang <landenlabs2@gmail.com>
2026-08-16 11:39:45 -0400

    cleanup, remove unused files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ import java.util.Properties
 
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
 }
 
 val localProperties = Properties().apply {
@@ -42,9 +41,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
     }
 }
 
